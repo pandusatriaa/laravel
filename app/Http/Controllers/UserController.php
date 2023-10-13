3bfd4bed743534143e862c7536ed2896d0077feb
fadhiilabiyyi <fadhiilabiyyitamsil007@gmail.com>
2023-10-13 14:35:28 +0700

    komentar

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\user;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $user = user::all();
+        return view('user.index', compact('user'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('user.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function daftar(Request $request)
+    {
+        $request -> validate([
+            'name' => 'required',
+            'email' => 'required',
+            'password' => '',
+            'role' => 'required'
+
+        ]);
+
+        
+        $email = $request->email; // Anda harus mengganti ini dengan cara sesuai dengan form Anda
+        $username = $request->name; // Anda harus mengganti ini dengan cara sesuai dengan form Anda
+
+        $email_first_3_letters = substr($email, 0, 3);
+        $username_first_3_letters = substr($username, 0, 3);
+        $password = $email_first_3_letters . $username_first_3_letters;
+
+
+
+
+        user::create([
+
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => $password,
+            'role' => $request->role,
+        ]);
+
+        return redirect()->back()->with('success', 'Berhasil menambahkan data user!');
+    }
+
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(user $user)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit($id)
+    {
+        $user = user::find($id);
+        return view('user.edit', compact('user'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request,  $id)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required',
+            'role' => 'required',
+            'password' => '',
+
+        ]);
+
+
+        $email = $request->email; // Anda harus mengganti ini dengan cara sesuai dengan form Anda
+        $username = $request->name; // Anda harus mengganti ini dengan cara sesuai dengan form Anda
+
+        $email_first_3_letters = substr($email, 0, 3);
+        $username_first_3_letters = substr($username, 0, 3);
+        $password = $email_first_3_letters . $username_first_3_letters;
+
+
+        user::where('id', $id)->update([
+            'name' => $request->name,
+            'email' => $request->email,
+            'role' => $request->role,
+            'password' => $password,
+
+        ]);
+
+        return redirect()->route('user.home')->with('success', 'Berhasil mengubah data!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($id)
+    {
+        user::where('id', $id)->delete();
+        return redirect()->back()->with('deleted', 'Berhasil menghapus data!');
+    }
+}
---
+++ app/Models/Order.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id', 'medicines', 'nama_customer', 'total_price'
+    ];
+
+    protected $casts = [
+        'medicines' => 'array',
+    ];
+}
